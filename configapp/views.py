a39cabb7167a29ddd8675e7ab2df542bcfec0ea7
ulugbek <udavlatboyev@gmail.com>
2025-10-31 12:08:20 +0500

    django modellar, Orm

--- configapp/views.py
+++ configapp/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    news = News.objects.all()
+    category = Category.objects.all()
+    content ={
+        "news":news,
+        "category":category
+    }
+    return render(request, 'index.html', content)
+
+def category(request):
+    category = Category.objects.all()
+    content = {
+        "category": category
+    }
+    return render(request, 'category.html', content)
